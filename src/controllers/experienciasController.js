1666d291b5bcb7b9ca85bdc200a130a75be84a5d
Luiz Gabriel <luizgabrielb.lima@gmail.com>
2023-06-03 19:01:11 -0300

    first commit

---
+++ src/controllers/experienciasController.js
@@ -0,0 +1,36 @@
+const experienciasRepository = require("../repositories/experienciasRepository");
+
+exports.getAllExperiencias = async (req, res) => {
+    const experiencias = await experienciasRepository.getAllExperiencias();
+    res.json(experiencias);
+};
+
+exports.getExperienciaById = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const experiencia = await experienciasRepository.getExperienciaById(id);
+    res.json(experiencia);
+};
+
+exports.createExperiencia = async (req, res) => {
+    const experiencia = req.body;
+    const newExperiencia = await experienciasRepository.createExperiencia(
+        experiencia
+    );
+    res.json(newExperiencia);
+};
+
+exports.updateExperiencia = async (req, res) => {
+    const id = parseInt(req.params.id);
+    const experiencia = req.body;
+    const updatedExperiencia = await experienciasRepository.updateExperiencia(
+        id,
+        experiencia
+    );
+    res.json(updatedExperiencia);
+};
+
+exports.deleteExperiencia = async (req, res) => {
+    const id = parseInt(req.params.id);
+    await experienciasRepository.deleteExperiencia(id);
+    res.json({ message: `Experiencia ${id} deleted` });
+};
